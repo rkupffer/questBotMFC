15a26b27add44a0b0f99c7421c1b3eaac65c4d07
Rebekka Kupffer <kupffer@uni-landau.de>
2022-10-03 15:09:08 +0200

    add try except to avoid errors on shuffled questionnaire pages

--- main.py
+++ main.py
@@ -2,17 +2,16 @@
 import random
 import sys
 import time
-import os
-import selenium
+
+import selenium.common.exceptions
 from selenium import webdriver
-from selenium.webdriver.common import by
 from selenium.webdriver import ActionChains
 
 # webdriver configurations
 # initialization of class chromeOptions and set path to google chrome
 options = webdriver.ChromeOptions()
 options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-options.add_argument("window-size=1000x1000")
+options.add_argument("window-size=700x700")
 
 # initialization of class Chrome and set questionnaire url
 quest_file = open("link2questionnaire.txt", "r")
@@ -33,19 +32,22 @@ def dragdrop(page, item, rank):
 
 all_ranks = ["1", "2", "3"]
 all_items = ["1", "2", "3"]
-quest_pages = ["01", "02", "03", "04", "05", "06"]
+quest_pages = ["02", "01", "03", "04", "05", "06", "07", "08"]
 p = 0
-
 while p < len(quest_pages):
-    time.sleep(0.5)
-    sampled_rank = random.sample(all_ranks, 3)
-    sampled_item = random.sample(all_items, 3)
-    for i in range(len(all_items)):
-        dragdrop(quest_pages[p], sampled_item[i], sampled_rank[i])
-        rand_pause_between_items = random.uniform(0.5, 3)
-        time.sleep(rand_pause_between_items)
-    chrome.find_element("name", "submitNext").click()
+    for page in quest_pages:
+        try:
+            sampled_rank = random.sample(all_ranks, 3)
+            sampled_item = random.sample(all_items, 3)
+            for i in range(len(all_items)):
+                dragdrop(page, sampled_item[i], sampled_rank[i])
+                rand_pause_between_items = random.uniform(0.5, 1)
+                time.sleep(rand_pause_between_items)
+            chrome.find_element("name", "submitNext").click()
+        except selenium.common.exceptions.NoSuchElementException:
+            pass
     p += 1
+
 
 
 chrome.close()
